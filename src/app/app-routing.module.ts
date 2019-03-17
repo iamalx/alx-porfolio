c4b804fd13f1850612ea29903d1bdc99691b15d1
Alex <reyesgx11@gmail.com>
2019-03-17 13:04:50 -0700

    pdf router works on desktop => mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResumeComponent } from './resume/resume.component'
+import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent },
   {path: 'resume', component: ResumeComponent}
 ];
 
